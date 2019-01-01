02478db74ee513196c72dffbf552a98f635d96a6
t.thiticate <t.thiticate@unesco.org>
2019-01-01 23:05:45 +0700

    fix error

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,5 +2,5 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
-
+console.log('process.env.MONGODB_URI : ' + process.env.MONGODB_URI);
 module.exports = {mongoose};
